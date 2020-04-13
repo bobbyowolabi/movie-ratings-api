bd634b0a6f128642e1d5fb98c6ec8a19ab06db6d
Bobby Owolabi <bobby@bobbyowolabi.com>
2020-04-13 09:39:50 -0700

    Added title ratings test to IMDbDatasetDownloaderTest

--- src/test/java/com/owodigi/util/IMDbDatasetDownloaderTest.java
+++ src/test/java/com/owodigi/util/IMDbDatasetDownloaderTest.java
@@ -30,15 +30,26 @@ public class IMDbDatasetDownloaderTest {
             Arrays.asList("tt0000001", "short", 	"Carmencita", "Carmencita", "0", "1894", "\\N", "1", "Documentary,Short"),
             Arrays.asList("tt0000002", "short", 	"Le clown et ses chiens", "Le clown et ses chiens", "0", "1892", "\\N", "5", "Animation,Short"),
             Arrays.asList("tt0000003", "short", "Pauvre Pierrot", "Pauvre Pierrot", "0", "1892", "\\N", "4", "Animation,Comedy,Romance")
-        );        
+        );
         testDownload(fileName, expected);
     }
-    
+
+    @Test
+    public void downloadTitleRatings() throws IOException {
+        final String fileName = "title.ratings.tsv.gz";
+        final List<List<String>> expected = AssertUtils.asList(
+            Arrays.asList("tt0000001", "5.6", "1593"),
+            Arrays.asList("tt0000002", "6.0", "195"),
+            Arrays.asList("tt0000003", "6.5", "1266")
+        );
+        testDownload(fileName, expected);
+    }
+
     /**
-     * 
+     *
      * @param fileName
      * @param expected
-     * @throws IOException 
+     * @throws IOException
      */
     private void testDownload(final String fileName, final List<List<String>> expected) throws IOException {
         mockServerClient
@@ -53,12 +64,12 @@ public class IMDbDatasetDownloaderTest {
         final Iterator<List<String>> expectedIterator = expected.iterator();
         final URL url = new URL("http://localhost:" + mockServerRule.getPort() + "/" + fileName);
         IMDbDatasetDownloader.read(url, new TitleBasicsFormat(), new IMDbDownloaderCallback() {
-            
+
             @Override
             public void read(final CSVRecord actual) {
                 Assert.assertTrue("Unexpected record " + actual, expectedIterator.hasNext());
                 AssertUtils.assertEquals(expectedIterator.next(), actual);
             }
-        });        
+        });
     }
 }
